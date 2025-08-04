4cd5e70be07b3574d317c95d1ead30b41424d23f
Andreas Gammelgaard Damsbo <andreas@gdamsbo.dk>
2025-08-04 12:29:30 +0200

    docs: initial book setup and rendering for English, Kiswahili and Danish

---
+++ publishing.R
@@ -0,0 +1,23 @@
+## Init book
+## 
+
+# babelquarto::quarto_multilingual_book(
+#   parent_dir = ".",
+#   project_dir = "book",
+#   main_language = "en",
+#   further_languages = c("da", "sw"),
+#   register_languages = TRUE,
+#   site_url = "https://knowledge.freesearchr.org",
+#   placement = c("sidebar", "navbar")
+# )
+
+## Rendering the multilanguage book
+babelquarto::render_book(file.path(".", "book"),)
+
+
+an <- autonewsmd$new(repo_name = "FreesearchR-knowledge")
+an$generate()
+
+an$write(force = TRUE)
+
+
